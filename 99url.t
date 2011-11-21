b73ed719910e2dfcde94ce46d0ea22ba5ec0e88d
Bertrand Mansion <bmansion@mamasam.com>
2011-11-21 20:15:41 +0100

    Still some normalization work to do

---
+++ 99url.t
@@ -0,0 +1,175 @@
+#!/usr/bin/env lua
+require 'Test.More'
+
+local url = require'neturl'
+d = require'dumper'
+dump = function(str, var) print(d.dump(str, var)) end
+
+local s
+local q
+
+plan(14)
+
+local u = url.parse("http://www.example.com")
+u.query.net = "url"
+is("http://www.example.com/?net=url", tostring(u), "Test new query variable")
+
+u.query.net = "url 2nd try"
+is("net=url+2nd+try", tostring(u.query), "Test build query")
+is("http://www.example.com/?net=url+2nd+try", tostring(u), "Test new query variable 2")
+
+u = url.parse("http://www.example.com/?last=mansion&first=bertrand&test=more")
+is("http://www.example.com/?first=bertrand&last=mansion&test=more", tostring(u), "Test sorted query")
+u.query.test = nil
+is("http://www.example.com/?first=bertrand&last=mansion", tostring(u), "Test remove query parameter 1")
+u.query.first = nil
+is("http://www.example.com/?last=mansion", tostring(u), "Test remove query parameter 2")
+u.query.last = nil
+is("http://www.example.com/", tostring(u), "Test remove query parameter 3")
+
+u = url.parse("http://www.example.com/")
+u:setQuery("dilly%20all.day&flapdoodle")
+is("http://www.example.com/?dilly_all.day&flapdoodle", tostring(u), "Test space in query parameters")
+
+u.query = u.parseQuery("start=10&test[0][first][1.1][20]=coucou")
+is("http://www.example.com/?start=10&test[0][first][1.1][20]=coucou", tostring(u), "Test query with brackets")
+ok("10" == u.query.start, "Test query values with brackets")
+ok("coucou" == u.query.test[0]["first"]["1.1"][20], "Test query values with brackets")
+
+
+u = url.parse("http://example.com/")
+is("http", u.scheme, "Test scheme http")
+u.scheme = "gopher"
+is("gopher://example.com/", tostring(u), "Test scheme gopher")
+
+u.fragment = "lua"
+is("gopher://example.com/#lua", tostring(u), "Test fragment")
+
+
+local test1 = {
+	["g:h"] = "g:h",
+	["g"] = "http://a/b/c/g",
+	["./g"] = "http://a/b/c/g",
+	["g/"] = "http://a/b/c/g/",
+	["/g"] = "http://a/g",
+	["//g"] = "http://g",
+	["?y"] = "http://a/b/c/d;p?y",
+	["g?y"] = "http://a/b/c/g?y",
+	["#s"] = "http://a/b/c/d;p?q#s",
+	["g#s"] = "http://a/b/c/g#s",
+	["g?y#s"] = "http://a/b/c/g?y#s",
+	[";x"] = "http://a/b/c/;x",
+	["g;x"] = "http://a/b/c/g;x",
+	["g;x?y#s"] = "http://a/b/c/g;x?y#s",
+	[""] = "http://a/b/c/d;p?q",
+	["."] = "http://a/b/c/",
+	["./"] = "http://a/b/c/",
+	[".."] = "http://a/b/",
+	["../"] = "http://a/b/",
+	["../g"] = "http://a/b/g",
+	["../.."] = "http://a/",
+	["../../"] = "http://a/",
+	["../../g"] = "http://a/g",
+	["../../../g"] = "http://a/g",
+	["../../../../g"] = "http://a/g",
+	["/./g"] = "http://a/g",
+	["/../g"] = "http://a/g",
+	["g."] = "http://a/b/c/g.",
+	[".g"] = "http://a/b/c/.g",
+	["g.."] = "http://a/b/c/g..",
+	["..g"] = "http://a/b/c/..g",
+	["./../g"] = "http://a/b/g",
+	["./g/."] = "http://a/b/c/g/",
+	["g/./h"] = "http://a/b/c/g/h",
+	["g/../h"] = "http://a/b/c/h",
+	["g;x=1/./y"] = "http://a/b/c/g;x=1/y",
+	["g;x=1/../y"] = "http://a/b/c/y",
+	["g?y/./x"] = "http://a/b/c/g?y%2F.%2Fx",
+	["g?y/../x"] = "http://a/b/c/g?y%2F..%2Fx",
+	["g#s/./x"] = "http://a/b/c/g#s/./x",
+	["g#s/../x"] = "http://a/b/c/g#s/../x",
+}
+
+for k,v in pairs(test1) do
+  local u = url.parse('http://a/b/c/d;p?q')
+  local res = u:resolve(k)
+  is(tostring(res), v, "Test resolve '".. k.."' => '"..v..' => '..tostring(res))
+end
+
+local test2 = {
+	["/foo/bar/."] = "/foo/bar/",
+["/foo/bar/./"] = "/foo/bar/",
+["/foo/bar/.."] = "/foo/",
+["/foo/bar/../"] = "/foo/",
+["/foo/bar/../baz"] = "/foo/baz",
+["/foo/bar/../.."] = "/",
+["/foo/bar/../../"] = "/",
+["/foo/bar/../../baz"] = "/baz",
+["/./foo"] = "/foo",
+["/foo."] = "/foo.",
+["/.foo"] = "/.foo",
+["/foo.."] = "/foo..",
+["/..foo"] = "/..foo",
+["/./foo/."] = "/foo/",
+["/foo/./bar"] = "/foo/bar",
+["/foo/../bar"] = "/bar",
+["/foo//"] = "/foo/",
+["/foo///bar//"] = "/foo/bar/",
+--["http://www.foo.com:80/foo"] = "http://www.foo.com/foo",
+["http://www.foo.com/foo/../foo"] = "http://www.foo.com/foo",
+["http://www.foo.com:8000/foo"] = "http://www.foo.com:8000/foo",
+--["http://www.foo.com./foo/bar.html"] = "http://www.foo.com/foo/bar.html",
+--["http://www.foo.com.:81/foo"] = "http://www.foo.com:81/foo",
+["http://www.foo.com/%7ebar"] = "http://www.foo.com/~bar",
+["http://www.foo.com/%7Ebar"] = "http://www.foo.com/~bar",
+--["http://www.example.com./"] = "http://www.example.com/",
+["-"] = "-",
+["http://www.foo.com/?p=529&#038;cpage=1#comment-783"] = "http://www.foo.com/?p=529&",
+["/foo/bar/../../../baz"] = "/baz",
+["/foo/bar/../../../../baz"] = "/baz",
+["/./../foo"] = "/foo",
+["/../foo"] = "/foo",
+}
+
+for k,v in pairs(test2) do
+  local u = url.parse(k):normalize()
+  is(tostring(u), v, "Test normalize '".. k .."' => '".. v .."' => '"..tostring(u).."'")
+end
+
+
+
+
+
+
+
+
+--[[
+
+
+local test2 = {
+	["http://:@example.com/"] = "http://example.com/",
+	["http://@example.com/"] = "http://example.com/",
+	["http://example.com"] = "http://example.com/",
+	["HTTP://example.com/"] = "http://example.com/",
+	["http://EXAMPLE.COM/"] = "http://example.com/",
+	["http://example.com/%7Ejane"] = "http://example.com/~jane",
+	["http://example.com/?q=%C3%87"] = "http://example.com/?q=%C3%87",
+	["http://example.com/?q=%E2%85%A0"] = "http://example.com/?q=%E2%85%A0",
+	["http://example.com/?q=%5c"] = "http://example.com/?q=%5C",
+	["http://example.com/?q=%5C"] = "http://example.com/?q=%5C",
+	["http://example.com/a/../a/b"] = "http://example.com/a/b",
+	["http://example.com/a/./b"] = "http://example.com/a/b",
+	["http://example.com:80/"] = "http://example.com/",
+	["http://example.com/"] = "http://example.com/",
+	["http://example.com/~jane"] = "http://example.com/~jane",
+	["http://example.com/a/b"] = "http://example.com/a/b",
+	["http://example.com:8080/"] = "http://example.com:8080/",
+	["http://user:password@example.com/"] = "http://user:password@example.com/"
+}
+
+for k,v in pairs(test2) do
+  local u = url.parse(k)
+  is(tostring(u), v, "Test normalize '".. k.."' => '"..v..' => '..tostring(u))
+end
+
+]]
---
+++ neturl.lua
@@ -0,0 +1,401 @@
+
+module('neturl', package.seeall)
+
+options = {
+  separator = '&'
+}
+
+-- @see http://www.iana.org/assignments/uri-schemes.html
+local services = {
+  ['acap']     = 674,
+  ['cap']      = 1026,
+  ['dict']     = 2628,
+  ['ftp']      = 21,
+  ['gopher']   = 70,
+  ['http']     = 80,
+  ['https']    = 443,
+  ['iax']      = 4569,
+  ['icap']     = 1344,
+  ['imap']     = 143,
+  ['ipp']      = 631,
+  ['ldap']     = 389,
+  ['mtqp']     = 1038,
+  ['mupdate']  = 3905,
+  ['news']     = 2009,
+  ['nfs']      = 2049,
+  ['nntp']     = 119,
+  ['rtsp']     = 554,
+  ['sip']      = 5060,
+  ['snmp']     = 161,
+  ['telnet']   = 23,
+  ['tftp']     = 69,
+  ['vemmi']    = 575,
+  ['afs']      = 1483,
+  ['jms']      = 5673,
+  ['rsync']    = 873,
+  ['prospero'] = 191,
+  ['videotex'] = 516,
+}
+
+local legal = {
+  ["-"] = true, ["_"] = true, ["."] = true, ["!"] = true,
+  ["~"] = true, ["*"] = true, ["'"] = true, ["("] = true,
+  [")"] = true, [":"] = true, ["@"] = true, ["&"] = true,
+  ["="] = true, ["+"] = true, ["$"] = true, [","] = true,
+  [";"] = true -- can be used for parameters in path
+}
+
+local function decode(str)
+  local str = str:gsub('+', ' ')
+  return (str:gsub("%%(%x%x)", function(c)
+      return string.char(tonumber(c, 16))
+  end))
+end
+
+local function encode(str)
+  return (str:gsub("([^A-Za-z0-9%_%.%-%~])", function(v)
+      return string.upper(string.format("%%%02x", string.byte(v)))
+  end))
+end
+
+-- for query values, prefer + instead of %20 for spaces
+local function encodeValue(str)
+  local str = encode(str)
+  return str:gsub('%%20', '+')
+end
+
+local function encodeSegment(s)
+  local legalEncode = function(c)
+    if legal[c] then
+      return c
+    end
+    return encode(c)
+  end
+  return s:gsub('([^a-zA-Z0-9])', legalEncode)
+end
+
+function build(self)
+  local url = ''
+  if self.path then
+    local path = self.path
+    path:gsub("([^/]+)", function (s) return encodeSegment(s) end)
+    url = url .. tostring(path)
+  end
+  if self.query then
+    local qstring = tostring(self.query)
+    if qstring ~= "" then
+      url = url .. '?' .. qstring
+    end
+  end
+  if self.host then
+    local authority = self.host
+    if self.port and self.scheme and services[self.scheme] ~= self.port then
+      authority = authority .. ':' .. self.port
+    end
+    local userinfo
+    if self.user then
+      userinfo = self.user
+      if self.password then
+        userinfo = userinfo .. ':' .. self.password
+      end
+    end
+    if userinfo then
+      authority = userinfo .. '@' .. authority
+    end
+    if authority then
+      if url ~= "" then
+        url = '//' .. authority .. '/' .. url:gsub('^/+', '')
+      else
+        url = '//' .. authority
+      end
+    end
+  end
+  if self.scheme then
+    url = self.scheme .. ':' .. url
+  end
+  if self.fragment then
+    url = url .. '#' .. self.fragment
+  end
+  return url
+end
+
+function buildQuery(tab, sep, key)
+  local query = {}
+  if not sep then
+    sep = options.separator or '&'
+  end
+  local keys = {}
+  for k in pairs(tab) do
+    keys[#keys+1] = k
+  end
+  table.sort(keys)
+  for _,name in ipairs(keys) do
+    local value = tab[name]
+    name = encode(tostring(name))
+    if key then
+      name = string.format('%s[%s]', tostring(key), tostring(name))
+    end
+    if type(value) == 'table' then
+      query[#query+1] = buildQuery(value, sep, name)
+    else
+      local value = encodeValue(tostring(value))
+      if value ~= "" then
+        query[#query+1] = string.format('%s=%s', name, value)
+      else
+        query[#query+1] = name
+      end
+    end
+  end
+  return table.concat(query, sep)
+end
+
+-- Historically, PHP had to replace brackets because of the infamous 
+-- register_globals parameter. We don't have this problem here, so
+-- we keep unbalanced brackets in key names.
+function parseQuery(str, sep)
+  if not sep then
+    sep = options.separator or '&'
+  end
+
+  local values = {}
+  for key,val in str:gmatch(string.format('([^%q=]+)(=*[^%q=]*)', sep, sep)) do
+    local key = decode(key)
+    local keys = {}
+    key = key:gsub('%[([^%]]*)%]', function(v)
+        -- extract keys between balanced brackets
+        if string.find(v, "^-?%d+$") then
+          v = tonumber(v)
+        else
+          v = decode(v)
+        end
+        table.insert(keys, v)
+        return "="
+    end)
+    key = key:gsub('=+.*$', "")
+    key = key:gsub('%s', "_") -- remove spaces in parameter name
+    val = val:gsub('^=+', "")
+
+    if not values[key] then
+      values[key] = {}
+    end
+    if #keys > 0 and type(values[key]) ~= 'table' then
+      values[key] = {}
+    elseif #keys == 0 and type(values[key]) == 'table' then
+      values[key] = decode(val)
+    end
+
+    local t = values[key]
+    for i,k in ipairs(keys) do
+      if type(t) ~= 'table' then
+        t = {}
+      end
+      if k == "" then
+        k = #t+1
+      end
+      if not t[k] then
+        t[k] = {}
+      end
+      if i == #keys then
+        t[k] = decode(val)
+      end
+      t = t[k]
+    end
+  end
+  setmetatable(values, { __tostring = _M.buildQuery })
+  return values
+end
+
+function setQuery(self, query)
+  local query = query
+  if type(query) == 'table' then
+    query = buildQuery(query)
+  end
+  self.query = parseQuery(query)
+  return query
+end
+
+function setAuthority(self, authority)
+  self.authority = authority
+  self.port = nil
+  self.host = nil
+  self.userinfo = nil
+  self.user = nil
+  self.password = nil
+
+  authority = authority:gsub('^([^@]*)@', function(v)
+    self.userinfo = v
+    return ''
+  end)
+  authority = authority:gsub(':([^:]*)$', function(v)
+      self.port = tonumber(v)
+      return ''
+  end)
+  if authority ~= '' then
+    self.host = authority:lower()
+  end
+  if self.userinfo then
+    local userinfo = self.userinfo
+    userinfo = userinfo:gsub(':([^:]*)$', function(v)
+        self.password = v
+        return ''
+    end)
+    self.user = userinfo
+  end
+  return authority
+end
+
+--- Parse the url into the designated parts.
+-- Depending on the url, the following parts can be available:
+-- scheme, userinfo, user, password, authority, host, port, path, 
+-- query, fragment
+-- @param url Url string
+-- @return A table with the different parts and a few other functions
+function parse(url)
+  local comp = {}
+  setAuthority(comp, "")
+  setQuery(comp, "")
+
+  local url = tostring(url or '')
+  url = url:gsub('#(.*)$', function(v)
+    comp.fragment = v
+    return ''
+  end)
+  url =url:gsub('^([%w][%w%+%-%.]*)%:', function(v)
+    comp.scheme = v:lower()
+    return ''
+  end)
+  url = url:gsub('^//([^/]*)', function(v)
+    setAuthority(comp, v)
+    return ''
+  end)
+  url = url:gsub('%?(.*)', function(v)
+    setQuery(comp, v)
+    return ''
+  end)
+  comp.path = decode(url)
+
+  setmetatable(comp, {
+    __index = _M,
+    __tostring =_M.build}
+  )
+  return comp
+end
+
+-- @see http://en.wikipedia.org/wiki/URL_normalization
+-- This function will also remove multiple slashes
+function removeDotSegments(path)
+  local fields = {}
+  if string.len(path) == 0 then
+    return ""
+  end
+  local startslash = false
+  local endslash = false
+  if string.sub(path, 1, 1) == "/" then
+    startslash = true
+  end
+  if (string.len(path) > 1 or startslash == false) and string.sub(path, -1) == "/" then
+    endslash = true
+  end
+
+  path:gsub('[^/]+', function(c) table.insert(fields, c) end)
+  
+  local new = {}
+  local j = 0
+  
+  for i,c in ipairs(fields) do
+    if c == '..' then
+      if j > 0 then
+        j = j - 1
+      end
+    elseif c ~= "." then
+      j = j + 1
+      new[j] = c
+    end
+  end
+  local ret = ""
+  if #new > 0 and j > 0 then
+    ret = table.concat(new, '/', 1, j)
+  else
+    ret = ""
+  end
+  if startslash then
+    ret = '/'..ret
+  end
+  if endslash then
+    ret = ret..'/'
+  end
+  return ret
+end
+
+local function absolutePath(base_path, relative_path)
+  if string.sub(relative_path, 1, 1) == "/" then 
+    return '/' .. string.gsub(relative_path, '^[%./]+', '')
+  end
+  local path = base_path
+  if relative_path ~= "" then
+    path = '/'..path:gsub("[^/]*$", "")
+  end
+  path = path .. relative_path
+  --path:gsub("/%.%.$", "/../")
+  --path:gsub("/%.$", "/./")
+  path:gsub("%.$", "./")
+  path = path:gsub("([^/]*%./)", function (s)
+    if s ~= "./" then return s else return "" end
+  end)
+  path = string.gsub(path, "/%.$", "/")
+  local reduced
+  while reduced ~= path do
+    reduced = path
+    path = string.gsub(reduced, "([^/]*/%.%./)", function (s)
+      if s ~= "../../" then return "" else return s end
+    end)
+  end
+  path = string.gsub(reduced, "([^/]*/%.%.)$", function (s)
+    if s ~= "../.." then return "" else return s end
+  end)
+  path = '/' .. string.gsub(path, '^/+%.%.?/?', '')
+
+  return path
+end
+
+function resolve(self, other)
+  if type(self) == "string" then
+    self = parse(self)
+  end
+  if type(other) == "string" then
+    other = parse(other)
+  end
+  if other.scheme then 
+    return other
+  else
+    other.scheme = self.scheme
+    if not other.authority or other.authority == "" then
+      other:setAuthority(self.authority)
+      if not other.path or other.path == "" then
+        other.path = self.path
+        local query = other.query
+        if not query or not next(query) then
+          other.query = self.query
+        end
+      else
+        other.path = absolutePath(self.path, other.path)
+      end
+    end
+    return other
+  end
+end
+
+function normalize(self)
+  if type(self) == 'string' then
+    self = parse(self)
+  end
+  if self.path then
+    local path = self.path
+    path = absolutePath(path, "")
+    -- normalize multiple slashes
+    path = string.gsub(path, "//+", "/")
+    -- 
+    self.path = path
+  end
+  return self
+end
